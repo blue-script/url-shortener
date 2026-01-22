95f0c4eb97369c3f47c8c668a9bebe3313a11544
blue-script <alexm92@inbox.ru>
2026-01-22 19:34:24 +0500

    feat: add request and response handlers to auth service

--- internal/auth/handler.go
+++ internal/auth/handler.go
@@ -2,9 +2,12 @@ package auth
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/blue-script/url-shortener/configs"
+	"github.com/blue-script/url-shortener/pkg/req"
+	"github.com/blue-script/url-shortener/pkg/res"
 )
 
 type AuthHandlerDeps struct {
@@ -24,18 +27,31 @@ func NewAuthHandler(router *http.ServeMux, deps AuthHandlerDeps) {
 }
 
 func (handler *AuthHandler) Login() http.HandlerFunc {
-	return func(w http.ResponseWriter, req *http.Request) {
-		fmt.Println(handler.Config.Auth.Secret)
-		fmt.Println("Login")
-		res := LoginResponse{
+	return func(w http.ResponseWriter, r *http.Request) {
+		body, err := req.HandleBody[LoginRequest](&w, r)
+		if err != nil {
+			log.Printf("Error: %v", err.Error())
+			return
+		}
+		fmt.Println(body)
+		data := LoginResponse{
 			Token: "123",
 		}
-		w
+		res.Json(w, data, 200)
 	}
 }
 
 func (handler *AuthHandler) Register() http.HandlerFunc {
-	return func(w http.ResponseWriter, req *http.Request) {
-		fmt.Println("register")
+	return func(w http.ResponseWriter, r *http.Request) {
+		body, err := req.HandleBody[RegisterRequest](&w, r)
+		if err != nil {
+			log.Printf("Error: %v", err.Error())
+			return
+		}
+		fmt.Println(body)
+		data := LoginResponse{
+			Token: "123",
+		}
+		res.Json(w, data, 200)
 	}
 }
